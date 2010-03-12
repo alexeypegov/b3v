5c25c024143786df679eb27a626aa98c8c5ddd67
spLeaner <spleaner@gmail.com>
2010-03-12 20:12:41 +0300

    markup description

---
+++ app/markup.py
@@ -0,0 +1,24 @@
+# (c) alexey pegov (spleaner@gmail.com) 2009-2010
+# 
+# markup description:
+#
+# -strike-through-
+# _italic_
+# *bold*
+#
+# http://somesite.com|title for <a href="http://somesite.com">title</a>
+# http://somesite.com|"long title" for <a href="http://somesite.com">long title</a>
+# http://somesite.com for <a href="http://somesite.com">http://somesite.com</a>
+# 
+# # first item
+# # second item
+# # third item
+#
+# * item
+# * another
+# * yet another one
+#
+# there are also a number of advanced things like youtube, vimeo, flickr or music mp3 files or images so
+# http://www.youtube.com/watch?a=GHJGD67I will be converted to an html with embedded player
+#
+#
